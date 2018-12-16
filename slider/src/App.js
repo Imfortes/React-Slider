10ac5bc62ddedd71faa3b503d739efe983c76520
Imfortes <imfortes@list.ru>
2018-12-16 13:01:23 +0300

    Третий комит

--- slider/src/App.js
+++ slider/src/App.js
@@ -1,60 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import Grid from 'react-bootstrap/lib/Grid';
-import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Col';
+import styled from 'styled-components';
 
-import styled from 'styled-components'
+import Header from './components/Header'
+import Menu from './components/Menu'
+import Main from './components/Main'
+
+const url = process.env.PUBLIC_URL + '/img/bg.png';
+
+const HeaderWrapper = styled.header`
+    width: 100%;
+    height: 49px;
+    background-color: #242424;
+    opacity: 0.8;
+`;
+
+const MenuWrapper = styled.div`
+    height: 89px;
+    padding-top: 20px;
+`;
+
+const MainWrapper = styled.main`
+    height: 600px;
+    padding-top: 130px;
+    background: url(${url}) no-repeat;
+    background-size: cover;
+`;
 
 class App extends Component {
   render() {
     return (
-        <div className={"wrapper"}>
-            <SayFullName name={"Александр"} surname={"Завершнев"} link={"https://vk.com/id7494002"} />
-            <SayFullName name={"Alexandr"} surname={"Zavershnev"} link={"https://vk.com/id7494002"} />
-            <SayFullName name={"Лекс"} surname={"Завершнев"} link={"https://vk.com/id7494002"} />
-            <CreateImage link={"img/broken-display-2222010_1920.png"} />
+        <div className="App">
+            <HeaderWrapper>
+                <Grid>
+                    <Header />
+                </Grid>
+            </HeaderWrapper>
+            <MenuWrapper>
+                <Grid>
+                    <Menu/>
+                </Grid>
+            </MenuWrapper>
+            <MainWrapper>
+                <Grid>
+                    <Main/>
+                </Grid>
+            </MainWrapper>
         </div>
     );
   }
 }
 
-function SayFullName(props){
-    return(
-        <div>
-            <h1>Мое имя {props.name}, моя фамилия - {props.surname}</h1>
-            <a href={props.link}>Ссылка на мой профиль</a>
-        </div>
-    )
-}
-
-function CreateImage(properties) {
-    return (
-        <div>
-            <img src={properties.link} alt=""/>
-        </div>
-    )
-}
 
 export default App;
 
 
-
-{/*<div className="App">*/}
-    {/*<header className="App-header">*/}
-        {/*<img src={logo} className="App-logo" alt="logo" />*/}
-        {/*<p>*/}
-            {/*Edit <code>src/App.js</code> and save to reload.*/}
-        {/*</p>*/}
-        {/*<a*/}
-            {/*className="App-link"*/}
-            {/*href="https://reactjs.org"*/}
-            {/*target="_blank"*/}
-            {/*rel="noopener noreferrer"*/}
-        {/*>*/}
-            {/*Learn React*/}
-        {/*</a>*/}
-    {/*</header>*/}
-{/*</div>*/}
